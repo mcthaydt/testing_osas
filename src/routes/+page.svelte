248a59a5bb27107ac8fff96f034e13b8f17dd95e
Dawson M <107786093+mcthaydt@users.noreply.github.com>
2024-09-10 23:34:09 -0500

    updated landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,37 @@
 <script>
+	import { AspectRatio } from '$lib/components/ui/aspect-ratio';
+	import { Badge } from '$lib/components/ui/badge';
 	import { Button } from '$lib/components/ui/button';
 	import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '$lib/components/ui/card';
 	import * as Select from '$lib/components/ui/dialog';
-	import { Github, Twitter, ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import { Github, Twitter, ChevronLeft, ChevronRight, ArrowUp, ExternalLink } from 'lucide-svelte';
 	import { onMount } from 'svelte';
+	import { flip } from 'svelte/animate';
+	import { products, upvote } from '$lib/stores/votingStore';
+	import { fade } from 'svelte/transition';
 
 	const features = [
-		{ title: 'Voting System', description: 'Assets are only added when users vote them in.' },
 		{
-			title: 'Open Source Focused',
+			title: 'User-Driven Voting System',
+			description: 'Ensures high-quality, community-approved assets that meet your needs.'
+		},
+		{
+			title: 'Exclusive Open Source Marketplace',
 			description:
-				'This marketplace is designed specifically for open source game engines without a dedicated asset store.'
+				'Access a curated collection of assets tailored specifically for open source game engines.'
 		},
 		{
-			title: 'Revenue Sharing',
-			description: 'Sellers can split their profits among contributors.'
+			title: 'Flexible Revenue Sharing Options',
+			description: 'Collaborate with ease and fairly compensate all contributors to your assets.'
 		},
 		{
-			title: 'Low Distributed Transaction Fee',
-			description: 'You keep 92%. We keep 5%. We donate 3% to open source game engines.'
+			title: 'Competitive 8% Transaction Fee',
+			description: 'Keep more of your earnings while supporting open source game development.'
 		}
 	];
 
 	const featuredAsset = {
-		name: 'This asset is just a placeholder',
+		name: 'This collection is just a placeholder',
 		description: "We're looking for a real one though 👀",
 		image:
 			'https://images.pexels.com/photos/27180675/pexels-photo-27180675/free-photo-of-perito-moreno-glacier-in-patagonia.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load'
@@ -42,14 +50,36 @@
 		localStorage.setItem('cookieConsent', 'true');
 		showCookieConsent = false;
 	}
+
+	$: sortedProducts = [...$products]
+		.sort((a, b) => {
+			return a.upvotes - b.upvotes;
+		})
+		.slice(0, 2);
+	$: signupCount = 0;
 </script>
 
 <main class="container mx-auto px-4">
 	<section class="hero py-20 text-center">
-		<h1 class="mb-4 text-5xl font-semibold tracking-normal">Open Source Asset Store</h1>
-		<p class="mb-8 text-xl">
+		<Badge variant="outline" class="mb-4">Over {signupCount} Prelaunch Signups</Badge>
+		<h1 class="mb-4 text-4xl font-semibold tracking-normal">
+			Asset Store for Open Source Game Engines
+		</h1>
+		<p class="mb-8 text-lg">
 			Access high-quality, free and paid assets to bring your game vision to life
 		</p>
+		<div class="mx-auto mb-12 w-[425px] rounded-xl">
+			<AspectRatio ratio={16 / 9} class="rounded-xl bg-muted">
+				<!-- svelte-ignore a11y_media_has_caption -->
+				<!-- svelte-ignore element_invalid_self_closing_tag -->
+				<video
+					src="https://media.istockphoto.com/id/1499384294/ko/비디오/바다에서-수영하는-돌고래의-수중-영상.mp4?s=mp4-480x480-is&k=20&c=tf43Gnm98xXTQYP1O7gWsV1gQTWIXJAw7TwtsfU71nU="
+					class="rounded-xl object-cover"
+					autoplay
+					muted
+				/>
+			</AspectRatio>
+		</div>
 		<div class="mt-4 flex justify-center">
 			<div class="flex flex-col items-center gap-4 sm:flex-row">
 				<a href="/godot">
@@ -80,11 +110,8 @@
 					</CardContent>
 					<CardFooter>
 						<div class="flex flex-col gap-2 md:flex-row">
-							<a href="https://forms.gle/p8EMx4DoQ3axtvEm9">
-								<Button class="min-w-[150px]" variant="outline" size="lg">Join Waitlist</Button>
-							</a>
-							<a href="/product_item/1">
-								<Button class="min-w-[150px]" variant="secondary" size="lg">View More</Button>
+							<a href="#/">
+								<Button variant="outline" size="lg">View Collection</Button>
 							</a>
 						</div>
 					</CardFooter>
@@ -109,11 +136,64 @@
 		</div>
 	</section>
 
-	<section class="cta rounded-lg bg-primary-foreground p-16 text-center">
+	<section class="features container py-16">
+		<h2 class="mb-2 text-center text-3xl font-bold">Under 24 Hours Remaining</h2>
+		<p class="mb-12 text-center text-sm text-muted-foreground">
+			These assets have less than 24 hours to get above the minimum votes
+		</p>
+		<div class="grid grid-cols-1 gap-8 md:grid-cols-1 lg:grid-cols-2">
+			{#each sortedProducts as product (product.id)}
+				<div animate:flip={{ duration: 300 }} transition:fade={{ duration: 300 }}>
+					<Card class="mx-auto w-full max-w-md">
+						<CardContent class="flex flex-col space-y-4 p-4">
+							<AspectRatio ratio={16 / 9}>
+								<img
+									src={product.imageUrl}
+									alt={`Image of ${product.name}`}
+									class="h-full w-full rounded-md object-cover"
+								/>
+							</AspectRatio>
+							<div class="flex items-center space-x-4">
+								<div class="flex flex-col items-center">
+									<Button variant="outline" size="icon" on:click={() => upvote(product.id)}>
+										<ArrowUp class="h-4 w-4" />
+									</Button>
+									<span class="mt-1 text-sm font-medium">{product.upvotes}</span>
+								</div>
+								<div>
+									<h2 class="text-lg font-semibold">{product.name}</h2>
+									<p class="text-sm text-muted-foreground">
+										Released: {product.releaseDate.toLocaleDateString()}
+									</p>
+									<p class="mt-2 text-sm">{product.description}</p>
+								</div>
+							</div>
+						</CardContent>
+						<CardFooter>
+							<Button variant="outline" class="w-full" href={`/voting_item/${product.id}`}>
+								<ExternalLink class="mr-2 h-4 w-4" />
+								View Product
+							</Button>
+						</CardFooter>
+					</Card>
+				</div>
+			{/each}
+		</div>
+	</section>
+
+	<section class="cta mb-12 rounded-lg bg-primary-foreground p-16 text-center">
+		<h2 class="mb-4 text-3xl font-bold">Any questions?</h2>
+		<p class="mb-8">For Frequently Asked Questions, Legal Information, and Contact</p>
+		<a href="support/">
+			<Button variant="secondary" size="lg">Visit Support</Button>
+		</a>
+	</section>
+
+	<section class="cta rounded-lg bg-secondary p-16 text-center">
 		<h2 class="mb-4 text-3xl font-bold">Sharing is Caring</h2>
 		<p class="mb-8">We'll let you know when we launch</p>
 		<a href="https://forms.gle/p8EMx4DoQ3axtvEm9">
-			<Button variant="secondary" size="lg">Join Waitlist</Button>
+			<Button variant="default" size="lg">Join Waitlist</Button>
 		</a>
 	</section>
 
